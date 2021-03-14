61c33b9922fab4bffcd52636e057ae90396ec2c0
Mersad Ajanovic <mersad.ajanovic@googlemail.com>
2021-03-14 16:29:54 +0100

    created question class to hold question and answer

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -28,18 +29,18 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
 
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.'
+  List<Question> questions = [
+    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
+    Question(
+        q: 'Approximately one quarter of human bones are in the feet.',
+        a: true),
+    Question(q: 'A slug\'s blood is green.', a: true),
   ];
-
-  List<bool> answers = [false, true, true];
 
   int questionAt = 0;
 
   Icon checkAnswer(bool userAnswer) {
-    if (userAnswer == answers[questionAt]) {
+    if (userAnswer == questions[questionAt].questionAnswer) {
       return Icon(Icons.check, color: Colors.green);
     } else {
       return Icon(Icons.close, color: Colors.red);
@@ -58,7 +59,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionAt],
+                questions[questionAt].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -84,7 +85,7 @@ class _QuizPageState extends State<QuizPage> {
               onPressed: () {
                 //The user picked true.
                 setState(() {
-                  if (questionAt >= questions.length) {
+                  if (questionAt < questions.length) {
                     scoreKeeper.add(checkAnswer(true));
                   }
                   if (questionAt < questions.length - 1) {
